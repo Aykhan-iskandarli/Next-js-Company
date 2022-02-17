65c51384d1723d803af497a9287bb59f611fac51
Aykhan <39117952+Aykhan-iskandarli@users.noreply.github.com>
2022-02-17 11:59:58 +0400

    layout test

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -8,19 +8,7 @@ function Layout({ children }) {
       <Head>
         <title>EliteBizz</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        {/* Roboto font*/}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
+     
       </Head>
       <Navbar />
       {children}
